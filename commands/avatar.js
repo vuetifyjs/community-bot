3438627fe91b9c0ed3976a989489ce37ecd24513
brandon pratt <bkpratt84@gmail.com>
2018-04-11 21:53:37 -0500

    Push deployment

--- commands/avatar.js
+++ commands/avatar.js
@@ -18,7 +18,6 @@ exports.run = async (client, message, args, level) => {
         if (client.config.defaultSettings.allowDeleteMessages === "true") client.log("log", "No permission to delete message", "Command");
     }
 
-
     let channelID = args[1].replace(/[#<>]/gi, '');
     let channel = client.channels.get(channelID);
 
